7fb31cc082ea8448a625695d798365b485d28a97
lettergram <lettergramm@gmail.com>
2020-01-16 02:06:00 -0600

    trying to fix xml parser

--- parse_patent.py
+++ parse_patent.py
@@ -1,4 +1,36 @@
 import xml.etree.ElementTree as etree
+import html
+
+test_str = "<NAM><ONM><STEXT><PDAT>Oblon, Spivak, McClelland, Maier & Neustadt, P.C.</PDAT></STEXT></ONM></NAM>"
+
+test_str = test_str.replace("&", "and")
+print(test_str)
+
+xml_tree = etree.fromstring(test_str, etree.XMLParser(encoding='utf-8'))
+print(xml_tree)
+
+
+
 filename = '20020101/patent-1888.xml'
-xml_file = open(filename, 'r')
-xml_tree = etree.parse(xml_file.read())
+
+xml_text = html.unescape(open(filename, 'r').read())
+xml_text = xml_text.replace("&", "and")
+xml_text = xml_text.replace("+", "")
+
+print(type(xml_text))
+count = 1
+for line in xml_text.split("\n"):
+    
+    print(count, line)
+    count += 1
+    if count == 371:
+        subcount = 0
+        for char in line:
+            subcount += 1
+            if subcount > 820 and subcount < 850:
+                print(subcount, char)
+        break
+
+
+xml_tree = etree.fromstring(xml_text, etree.XMLParser(encoding='utf-8'))
+print(xml_tree)
