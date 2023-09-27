998e45aac9060e0a36f45a36645067418106e415
JGSweets <JGSweets@users.noreply.github.com>
2023-09-26 20:20:14 -0500

    refactor: abstract loading xml data to function (#15)
    * refactor: abstract loading xml data to function
    
    * fix: missing init
    
    * fix: logger

--- parse_uspto_xml/setup_loggers.py
+++ parse_uspto_xml/setup_loggers.py
@@ -1,32 +1,57 @@
+from __future__ import annotations
+
 import os
 import sys
 import logging
 from pathlib import Path
 
 
-logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+_logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
 
 
-def setup_root_logger(level: int = logging.INFO):
+def create_file_handler(log_name):
+    """creates a file logger"""
+    log_dirpath = os.getcwd()
+    log_filepath = os.path.join(
+        log_dirpath,  "logs", os.path.basename(log_name) + ".log"
+    )
+    Path(os.path.dirname(log_filepath)).mkdir(parents=True, exist_ok=True)
+    file_handler = logging.FileHandler(filename=log_filepath)
+    file_handler.setFormatter(_logFormatter)
+    return file_handler
+
+
+def setup_root_logger(level: int = logging.INFO, include_file_logger: bool = False):
     """Sets up root logger to use console."""
     root_logger = logging.getLogger()
+
+    # don't setup root again if already setup.
+    for handler in root_logger.handlers:
+        if handler.stream == sys.stdout:
+            return
+
     handler = logging.StreamHandler(sys.stdout)
-    handler.setFormatter(logFormatter)
+    handler.setFormatter(_logFormatter)
     root_logger.setLevel(level)
     root_logger.addHandler(handler)
 
+    if include_file_logger:
+        file_handler = create_file_handler("main")
+        root_logger.addHandler(file_handler)
 
-def setup_file_logger(filename: str, level: int = logging.INFO) -> logging.Logger:
+
+def set_root_logger_level(level: int = logging.INFO):
+    """Sets the level for the root logger."""
+    root_logger = logging.getLogger()
+    root_logger.setLevel(level)
+
+
+def setup_file_logger(filename: str, level: int | None = None) -> logging.Logger:
     """Sets up file logger for an individual file."""
-    log_dirpath = os.path.join(os.getcwd(), "logs")
-    Path(log_dirpath).mkdir(parents=True, exist_ok=True)
+    level = logging.getLogger().level
 
-    log_filepath = os.path.join(
-        log_dirpath, os.path.basename(filename) + ".log"
-    )
+    file_handler = create_file_handler(filename)
     logger = logging.getLogger(filename)
-    file_handler = logging.FileHandler(filename=log_filepath)
-    file_handler.setFormatter(logFormatter)
     logger.setLevel(level)
     logger.addHandler(file_handler)
     return logger
